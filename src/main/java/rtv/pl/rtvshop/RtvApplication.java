a0c6b4a74da9a596d4db9a9bb1fd63457a6b08e2
namlev <mikipatkowski@protonmail.com>
2024-03-02 16:27:58 +0100

    Transfer to Spring Boot application

---
+++ src/main/java/rtv/pl/rtvshop/RtvApplication.java
@@ -0,0 +1,13 @@
+package rtv.pl.rtvshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rtv/pl/rtvshop/RtvApplicationTests.java
@@ -0,0 +1,13 @@
+package rtv.pl.rtvshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
