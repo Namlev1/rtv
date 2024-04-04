298c00db73a3d3cd50d3c767838cd04d0d92c835
namlev <mikipatkowski@protonmail.com>
2024-04-04 21:46:57 +0200

    Implement contact page

---
+++ src/main/java/rtv/pl/rtvshop/ContactController.java
@@ -0,0 +1,15 @@
+package rtv.pl.rtvshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contact")
+public class ContactController {
+
+    @GetMapping
+    public String getContactPage() {
+        return "contact";
+    }
+}
