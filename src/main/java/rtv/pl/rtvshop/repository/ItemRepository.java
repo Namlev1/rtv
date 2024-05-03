192244a4c81dd9bcdcce5db10324cf623f8125a4
namlev <mikipatkowski@protonmail.com>
2024-05-03 13:09:49 +0200

    Improve search

--- src/main/java/rtv/pl/rtvshop/repository/ItemRepository.java
+++ src/main/java/rtv/pl/rtvshop/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package rtv.pl.rtvshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import rtv.pl.rtvshop.model.Category;
 import rtv.pl.rtvshop.model.Item;
@@ -11,7 +13,9 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findTop8ByAccessibilityIsTrue();
 
-    List<Item> findByNameContaining(String name);
-
     List<Item> findByCategory(Category categories);
+
+    @Query("SELECT i FROM Item i WHERE LOWER(i.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+    List<Item> findByNameContaining(@Param("name") String name);
 }
+
